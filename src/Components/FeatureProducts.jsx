c2f34a403355d4a7e108db56711a4f180aa378ed
JOYDEEP <joydeep.accounts@tuta.io>
2023-05-10 16:24:47 +0530

    Feature product & formatPrice completed

--- src/Components/FeatureProducts.jsx
+++ src/Components/FeatureProducts.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import Product from './Product';
+
+import "../SCSS/FeatureProducts.scss";
 
 import { useProductContext } from '../Context/ProductContext';
 
 const FeatureProducts = () => {
 
-    const { isLoading, featureProducts } = useProductContext() ;
+    const { isLoading, featureProducts } = useProductContext();
 
     return (
-        <div className='FeatureProducts'>
-
-        </div>
+        <>
+            {
+                isLoading
+                    ? <div className='loading'>...Loading</div>
+                    :
+                    <div className='FeatureProducts'>
+                        <div className='top-part'>
+                            <p>Check now!</p>
+                            <h3>Our feature products</h3>
+                        </div>
+                        <div className='feature-products'>
+                            {
+                                featureProducts.map( (item) => {
+                                    return <Product  key={item.id} {...item} />
+                                })
+                            }
+                        </div>
+                    </div>
+            }
+        </>
     )
 }
 
---
+++ src/Components/Product.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import FormatPrice from '../Helpers/FormatPrice';
+
+import "../SCSS/Product.scss";
+
+const Product = ({ ...item }) => {
+
+    const { id, name, image, price, category } = item;
+
+    return (
+        <Link to={`/singleproduct/${id}`} className='Product'>
+
+            <figure className='figure'>
+                <img src={image} alt={name} />
+                <figcaption>{category}</figcaption>
+            </figure>
+
+            <div className='cardData'>
+                <p>{name}</p>
+                <p><FormatPrice price={price} /></p>
+            </div>
+
+        </Link>
+    )
+}
+
+export default Product;
