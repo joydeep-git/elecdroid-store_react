ec1dca8ee613c7a783a18e6298aaf716d93cc645
JOYDEEP <joydeep.accounts@tuta.io>
2023-05-15 22:02:04 +0530

    extracted all company and category

--- src/Components/FilterSection.jsx
+++ src/Components/FilterSection.jsx
@@ -5,12 +5,24 @@ import { useFilterContext } from '../Context/filter_context';
 
 const FilterSection = () => {
 
-    const { filters:{text}, updateFilterValue } = useFilterContext();
+    const { filters: { text }, updateFilterValue, all_products } = useFilterContext();
+
+    const uniqueCategory = (data, property) => {
+
+        let newVal = data.map((e) => e[property]);
+
+        return newVal = ["All", ...new Set(newVal)];
+    }
+
+    const categoryOnlyData = uniqueCategory(all_products, "category");
+
+    const companyOnlyData = uniqueCategory( all_products, "company");
+
     return (
         <div className='FilterSection'>
             <div className="search">
-                <form onSubmit={ (e) => e.preventDefault() }>
-                    <input type="text" name='text' value={text} onChange={updateFilterValue}/>
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <input type="text" name='text' value={text} placeholder='Search' onChange={updateFilterValue} />
                 </form>
             </div>
         </div>
