909b1228a04d88ad179c0bb40eaed32bc14ab536
JOYDEEP DAS <joydeep.accounts@tuta.io>
2023-09-07 19:28:10 +0530

    removed category and added price

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -7,19 +7,22 @@ import "../SCSS/Product.scss";
 
 const Product = ({ ...item }) => {
 
-    const { id, name, image, price, category } = item;
+    const { id, name, image, price,
+        // category 
+    } = item;
 
     return (
         <Link to={`/singleproduct/${id}`} className='Product'>
 
             <figure className='figure'>
                 <img src={image} alt={name} />
-                <figcaption>{category}</figcaption>
+                {/* <figcaption className='fig1'>{category}</figcaption> */}
+                <figcaption className='fig2'><FormatPrice price={price} /></figcaption>
             </figure>
 
             <div className='cardData'>
                 <p>{name}</p>
-                <p><FormatPrice price={price} /></p>
+                {/* <p><FormatPrice price={price} /></p> */}
             </div>
 
         </Link>
